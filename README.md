50e18a2fddc18cf4ad5998bd26a922855d4689fb
aaronlin <aaronlin@kkbox.com>
2014-07-18 23:35:52 +0800

    add script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-coscup_2014
-===========
+Usage
+===
+
+1. Set up parameters in submit_job.py
+    - Specify the input data and the mrjob script
+    - Specify the number of instance you want to run and the instance type you want to try
+2. run `python submit_job.py`
+3. that's it!
---
+++ submit_job.py
@@ -0,0 +1,40 @@
+import subprocess
+import time
+import sys
+
+
+def execute_emr_job(label, python_file, input_file, master_type, core_type, num_core_nodes):
+
+    cmd = ['python', python_file, '-r', 'emr', input_file,
+            '--label', label,
+            '--ec2-master-instance-type', master_type,
+            '--ec2-core-instance-type', core_type,
+            '--num-ec2-core-instances', str(num_core_nodes),
+            '--file', 'play_count.txt',
+            '--no-output']
+
+    sys.stderr.write(' '.join(cmd) + '\n')
+    sys.stderr.flush()
+    subprocess.Popen(cmd, stdin=None, stdout=None, stderr=None, close_fds=True)  # run it in background
+
+    time.sleep(30)
+
+
+if __name__ == '__main__':
+    python_file = 'calc_similarity.py'
+    input_file = 's3://research-center/personal/aaronlin/mrjob/data/all/'
+    num_core_nodes = 5
+    # When you run multiple instances, the master node is just coordinating the
+    #   other nodes, so usually the default instance type (m1.small) is fine, and using larger instances is wasteful.
+    master_type = 'm1.small'
+
+    # use different configurations
+    core_types = ['m1.small', 'm1.medium', 'm1.large', 'm1.xlarge', 'm3.xlarge', 'm3.2xlarge',
+            'c1.medium', 'c1.xlarge', 'cc2.8xlarge', 'c3.xlarge', 'c3.2xlarge', 'c3.4xlarge', 'c3.8xlarge',
+            'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge', 'cr1.8xlarge', 'r3.xlarge', 'r3.2xlarge', 'r3.4xlarge', 'r3.8xlarge']
+    label = 'test'
+
+    # execute emr jobs
+    for core_type in core_types:
+        label = 'test-%s' % core_type
+        execute_emr_job(label, python_file, input_file, master_type, core_type, num_core_nodes)
